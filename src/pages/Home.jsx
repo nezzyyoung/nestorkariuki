eff3ee623cdf8bcb7e80fbdd155cd0bef3af1d14
nezzyyoung <nezzybucumi@gmail.com>
2025-05-14 15:55:12 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen ">
+
+        {/* Theme Toggle */}
+
+
+        {/* Background Effects */}
+
+
+        {/* NavBar */}
+
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+
+
+    </div>
+    );
+};
