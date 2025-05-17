866c1d5a1f14f3376c726fa2dd8b8b4d4531b3d7
nezzyyoung <nezzybucumi@gmail.com>
2025-05-17 19:36:48 +0300

    updates

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,64 @@
+
+
+const skills = [
+    //Frontend
+    {name: "Html/CSS", level: 96, category: "frontend"},
+    {name: "JavaScript", level: 98, category: "frontend"},
+    {name: "Next.js", level: 75, category: "frontend"},
+    {name: "React.js", level: 80, category: "frontend"},
+    {name: "Typescript", level: 76, category: "frontend"},
+    {name: "Expo", level: 96, category: "frontend"},
+    {name: "tailwind CSS", level: 96, category: "frontend"},
+
+    //Backend
+    {name: "Node.js", level: 75, category: "backend"},
+    {name: "Python", level: 95, category: "backend"},
+    {name: "MySQL", level: 96, category: "backend"},
+    {name: "MongoDB", level: 76, category: "backend"},
+    {name: "Serverless", level: 66, category: "backend"},
+    {name: "GraphQl", level: 66, category: "backend"},
+    {name: "Django", level: 66, category: "backend"},
+    {name: "PostgresSQL", level: 76, category: "backend"},
+
+    //Tools
+    {name: "Git/GitHub", level: 90, category: "tools"},
+    {name: "Figma", level: 85, category: "tools"},
+    {name: "Canvas", level: 70, category: "tools"},
+    {name: "Pinterest", level: 96, category: "tools"},
+    {name: "Vs Code", level: 96, category: "tools"},
+    {name: "Android Studio", level: 86, category: "tools"},
+    {name: "AWS", level: 86, category: "tools"},
+    
+]
+
+
+
+
+export const SkillsSection = () => {
+    return (
+        <section 
+            id="skills" 
+            className="py-24 px-4 relative bg-secondary/30"
+        >
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    My <span className="text-primary"> Skills </span>
+                </h2>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {skills.map((skill, key) => (
+                        <div 
+                            key={key} 
+                            className="bg-card p-6 rounded-lg shadow-xs card-hover"
+                        >
+                            <div className="text-left mb-4">
+                                <h3 className="font-semibold text-lg"> {skill.name} </h3>
+                            </div>
+                            <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden"></div>                     
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
