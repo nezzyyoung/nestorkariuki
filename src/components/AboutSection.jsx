c1cda585a57e50a8437eef0b535df50c85670c42
nezzyyoung <nezzybucumi@gmail.com>
2025-05-18 16:18:48 +0300

    user can now download my cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
                                 Get In Touch 
                             </a>
 
-                            <a href="#" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"> 
+                            <a href="/cv/mycv.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"> 
                                 Download CV 
                             </a>
                         </div>
