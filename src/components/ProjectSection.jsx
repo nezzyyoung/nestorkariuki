8cc94b1fa0b4500f300a0c42e23a7de73ee50fe4
nezzyyoung <nezzybucumi@gmail.com>
2025-05-18 15:04:43 +0300

    updates

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react"
 
 
 const projects = [
@@ -7,7 +8,8 @@ const projects = [
         description: "A seamless landing page for a solar energy online firm",
         image: "/projects/web1.png",
         tag: ["Wordpress", "Php"],
-        demoUrl: "rutsonenergy.com"
+        demoUrl: "https://rutsonenergy.com",
+        githubUrl: ""
     },
     {
         id: 3,
@@ -15,7 +17,8 @@ const projects = [
         description: "A seamless landing page for an ecommerce online app",
         image: "/projects/web2.png",
         tag: ["Javascript", "Node.js", "MySQL"],
-        demoUrl: "buibuiecommerce.netlify.app"
+        demoUrl: "https://buibuiecommerce.netlify.app",
+        githubUrl: ""
     },
     {
         id: 3,
@@ -23,7 +26,8 @@ const projects = [
         description: "A seamless landing page for a loyalty online firm",
         image: "/projects/web3.png",
         tag: ["Wordpress", "Php", "JavaScript"],
-        demoUrl: "ecodreum.com"
+        demoUrl: "https://ecodreum.com",
+        githubUrl: ""
     },
 ]
 
@@ -47,11 +51,57 @@ export const ProjectSection = () => {
                             className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
                         >
                             <div className="h-48 overflow-hidden">
-                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                                <img 
+                                    src={project.image} 
+                                    alt={project.title} 
+                                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                                />
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tag.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium rounded-full bg-secondary text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <h3 className="text-xl font-semibold mb-1"> {project.title} </h3>
+                            <p className="text-muted-foreground text-sm mb-4"> {project.description} </p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a 
+                                        href={project.demoUrl} 
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    > 
+                                        <ExternalLink size={20} /> 
+                                    </a>
+                                    <a 
+                                        href={project.githubUrl} 
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    > 
+                                        <Github size={20} /> 
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
+
+                <div className="text-center mt-12">
+                    <a 
+                        className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                        target="_blank"
+                        href="https://github.com/nezzyyoung"
+                    >
+                        Check My Github <ArrowRight size={16} />
+                    </a>
+                </div>
+                
             </div>
         </section>
     )
