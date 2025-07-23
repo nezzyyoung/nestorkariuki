34adc90365278e1bd69851960bce130689c05ba0
nezzyyoung <nezzybucumi@gmail.com>
2025-07-23 23:30:52 +0300

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
             <p className="text-sm text-muted-foreground"> 
-                &copy; {new Date().getFullYear()}  Senovara.co All Rights Reserved  
+                &copy; {new Date().getFullYear()}  Senovara.com All Rights Reserved  
                 
             </p>
         </footer>
